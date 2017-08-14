763e701f179063a59cf4309d3e2dd19a374602a4
Trejkaz (pen name) <trejkaz@trypticon.org>
2017-08-14 13:38:54 +0900

    Trying to fix compile

--- code_pages.c
+++ code_pages.c
@@ -10,40 +10,47 @@
 #define STR_SIZE 8
 
 void printMapping(char *multiByteString, int multiByteStringLength, wchar_t *wideCharString, int wideCharStringLength) {
+	int i;
+
 	fprintf(stdout, "0x");
-	for (int i = 0; i < multiByteStringLength; i++) {
+	for (i = 0; i < multiByteStringLength; i++) {
 		fprintf(stdout, "%02X", multiByteString[i]);
 	}
 	fprintf(stdout, "\t0x");
-	for (int i = 0; i < wideCharStringLength; i++) {
+	for (i = 0; i < wideCharStringLength; i++) {
 		fprintf(stdout, "%04X", wideCharString[i]);
 	}
 	fprintf(stdout, "\t#");
 }
 
 int main(int argv, char *argv[]) {
+	int codePage;
+	CPINFOEX cpInfo;
+	int success;
+	char multiByteString[STR_SIZE];
+	wchar_t wideCharString[STR_SIZE];
+	int onlyByte;
+	int resultSize;
+	int firstByte;
+	int secondByte;
 
 	//TODO: At top, should print the version of Windows being used.
 
-	for (int codePage = 0; codePage < 65536; codePage++) {
-		CPINFOEX cpInfo;
+	for (codePage = 0; codePage < 65536; codePage++) {
 
-		int success = GetCPInfo(codePage, &cpInfo);
+		success = GetCPInfo(codePage, &cpInfo);
 		if (success) {
 
 			fprintf(stdout, "\n# Code page %d (%s)\n\n", codePage, cpInfo.CodePageName);
 
 			//XXX: DefaultChar, UnicodeDefaultChar may be useful, maybe not. :/
 
-	    	char multiByteString[STR_SIZE];
-	    	wchar_t wideCharString[STR_SIZE];
-
 			switch (cpInfo.MaxCharSize) {
 				case 1:
-				    for (int onlyByte = 0; onlyByte < 256; onlyByte++) {
+				    for (onlyByte = 0; onlyByte < 256; onlyByte++) {
 				    	multiByteString[0] = onlyByte;
 
-				    	int resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 1, wideCharString, STR_SIZE);
+				    	resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 1, wideCharString, STR_SIZE);
 				    	if (resultSize > 0) {
 					    	printMapping(multiByteString, 1, wideCharString, resultSize);
 					    }
@@ -52,13 +59,13 @@ int main(int argv, char *argv[]) {
 
 				case 2:
 					//XXX: Could use cpInfo.LeadByte to get this information without making a call.
-				    for (int firstByte = 0; firstByte < 256; firstByte++) {
+				    for (firstByte = 0; firstByte < 256; firstByte++) {
 				        if (IsDBCSLeadByteEx(codePage, firstChar)) {
-				            for (int secondByte = 0; secondByte < 256; secondByte++) {
+				            for (secondByte = 0; secondByte < 256; secondByte++) {
 						    	multiByteString[0] = firstByte;
 						    	multiByteString[1] = secondByte;
 
-						    	int resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 2, wideCharString, STR_SIZE);
+						    	resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 2, wideCharString, STR_SIZE);
 						    	if (resultSize > 0) {
 							    	printMapping(multiByteString, 2, wideCharString, resultSize);
 							    }
@@ -66,7 +73,7 @@ int main(int argv, char *argv[]) {
 				        } else {
 					    	multiByteString[0] = firstByte;
 
-					    	int resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 1, wideCharString, STR_SIZE);
+					    	resultSize = MultiByteToWideChar(codePage, MB_ERR_INVALID_CHARS, multiByteString, 1, wideCharString, STR_SIZE);
 					    	if (resultSize > 0) {
 						    	printMapping(multiByteString, 1, wideCharString, resultSize);
 						    }
